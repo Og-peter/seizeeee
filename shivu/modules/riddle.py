f1b5bf8d14e25c977eb27006f213fccb4873ea63
Itachiuchiha786786 <166844972+Itachiuchiha786786@users.noreply.github.com>
2024-10-25 12:22:17 +0530

    Update riddle.py

--- shivu/modules/riddle.py
+++ shivu/modules/riddle.py
@@ -81,7 +81,7 @@ async def start_anime_guess_cmd(update: Update, context: CallbackContext):
 
 # Function to handle guess timeout
 async def guess_timeout(context: CallbackContext, chat_id: int):
-    await asyncio.sleep(40)
+    await asyncio.sleep(10)
 
     # Check if there's still an active game after timeout
     if chat_id in active_guesses:
