31008378b1cebe8adb69f569db6607bcb84339f8
Itachiuchiha786786 <166844972+Itachiuchiha786786@users.noreply.github.com>
2024-11-03 12:09:20 +0530

    Update gitpull.py

--- shivu/modules/gitpull.py
+++ shivu/modules/gitpull.py
@@ -19,7 +19,7 @@ async def git_pull_command(client, message):
     try:
         # Execute the git pull command with a timeout (to prevent hanging)
         result = subprocess.run(
-            ["git", "pull", "https://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@github.com/Itachiuchiha786786/seizeeee", "main"],
+            ["git", "pull", "https://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@github.com/Itachiuchiha786786/seizeeee", "main"],
             capture_output=True, text=True, check=True, timeout=60  # 60 seconds timeout
         )
 
