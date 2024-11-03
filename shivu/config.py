d1b78b71f9c99346a24ecf6ca009dde3a42f18fe
Itachiuchiha786786 <166844972+Itachiuchiha786786@users.noreply.github.com>
2024-11-03 12:56:53 +0530

    Update config.py

--- shivu/config.py
+++ shivu/config.py
@@ -5,10 +5,10 @@ class Development:
     # Define all the required configurations here
     api_id = 29098103
     api_hash = "06baef4020832888ccf3ebf4e746d52b"
-    TOKEN = "YOUR_BOT_TOKEN"  # Replace with your actual bot token
+    TOKEN = "7335799800:AAFzxuSNLeUFAFB-9XY4_EdbUdLUQLegbBQ"  # Replace with your actual bot token
     GROUP_ID = -1002104939708
     CHARA_CHANNEL_ID = -1002049694247
-    mongo_url = "YOUR_MONGO_URL"  # Replace with your actual MongoDB URL
+    mongo_url = "mongodb+srv://seizewaifubot:seizewaifubot@itachi.9qya0.mongodb.net/?retryWrites=true&w=majority&appName=itachi"  # Replace with your actual MongoDB URL
     PHOTO_URL = [
         "https://telegra.ph/file/c74151f4c2b56a107a24b.jpg",
         "https://telegra.ph/file/6a81a91aa4a660a73194b.jpg"
