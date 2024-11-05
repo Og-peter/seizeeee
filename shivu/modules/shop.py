ece8d9ed6e0bf7a024c1df065cea3b601458c522
Itachiuchiha786786 <166844972+Itachiuchiha786786@users.noreply.github.com>
2024-11-05 10:50:13 +0530

    Update shop.py

--- shivu/modules/shop.py
+++ shivu/modules/shop.py
@@ -3,7 +3,7 @@ from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, CallbackQ
 from pymongo import MongoClient
 from datetime import datetime
 import random
-from shivu import user_collection, item_collection
+from shivu import user_collection
 from shivu import shivuu as app
 
 # Constants
