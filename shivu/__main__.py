e802ba4e43f90e3ef4e85e000a064f7f72e5c80e
Itachiuchiha786786 <166844972+Itachiuchiha786786@users.noreply.github.com>
2024-11-27 15:53:21 +0530

    Update __main__.py

--- shivu/__main__.py
+++ shivu/__main__.py
@@ -5,7 +5,6 @@ import random
 import re
 import asyncio
 import math
-import info_callback
 from html import escape
 from telegram import InlineKeyboardButton, InlineKeyboardMarkup, Update
 from telegram.ext import CommandHandler, CallbackContext, MessageHandler, filters, ContextTypes, Application, CallbackQueryHandler, CallbackContext
