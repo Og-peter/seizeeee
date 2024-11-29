f38fb9c14078ffa2b849ae91d38ddbb1f4fa3943
Itachiuchiha786786 <166844972+Itachiuchiha786786@users.noreply.github.com>
2024-11-29 09:48:17 +0530

    Update claim.py

--- shivu/modules/claim.py
+++ shivu/modules/claim.py
@@ -7,7 +7,7 @@ import asyncio
 from pyrogram.errors.exceptions.bad_request_400 import UserNotParticipant
 from pyrogram.enums import ChatMemberStatus
 
-OWNER_ID = 123456789  # Replace with the owner user ID
+OWNER_ID = 6402009857  # Replace with the owner user ID
 DEVS = (6995317382,)
 SUPPORT_CHAT_ID = -1002104939708
 CHANNEL_ID = -1002122552289
