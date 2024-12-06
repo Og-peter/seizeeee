322811fcd432a9899d79490b0600146c0714c5c5
Itachiuchiha786786 <166844972+Itachiuchiha786786@users.noreply.github.com>
2024-12-06 11:16:23 +0530

    Update riddle.py

--- shivu/modules/riddle.py
+++ shivu/modules/riddle.py
@@ -13,8 +13,8 @@ user_streaks = {}
 character_message_links = {}
 
 # Allowed group and support group URL
-ALLOWED_GROUP_ID = -1002104939708  # Replace with your allowed group's chat ID
-SUPPORT_GROUP_URL = "https://t.me/dynamic_gangs"  # Replace with your actual support group URL
+ALLOWED_GROUP_ID = -1002466950912  # Replace with your allowed group's chat ID
+SUPPORT_GROUP_URL = "https://t.me/Dyna_community"  # Replace with your actual support group URL
 
 # Function to fetch a random anime character from the database
 async def get_random_character():
